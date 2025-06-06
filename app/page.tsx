ccd23cece8f474cd8b5d179d7a12ea2a19423d38
putuadityadev <adityamph1@gmail.com>
2025-06-06 12:01:26 +0800

    feat(layout): add responsive section container component

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
 export default function Home() {
   return (
    <main className="">
+    <section className="section-container">
       <h1 className="hero-text">Ini Adalah Hero Section Text</h1>
       <p className="p-hero-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus, eligendi.</p>
+    </section>
    </main>
   );
 }
